268e3e4ab2b778b43573300af592ce40e52e49bd
Pragati Jain <132778037+Pragati-3003@users.noreply.github.com>
2024-06-16 16:23:59 +0530

    Time: 51 ms (45.69%), Space: 25.1 MB (36.43%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,94 +1,33 @@
 class Solution {
 public:
-    
-    bool check(int i, int j, int n, int m,vector<vector<int>>& g,  vector<vector<bool>> &vis)
-    {
-        if(i>=n || j>=m |i<0 || j<0 || g[i][j]!=0 ||  vis[i][j])
-            return false;
-        return true;
-    }
-    
-    int shortestPathBinaryMatrix(vector<vector<int>>& g) {
-        int n  = g.size();
-        int m = g[0].size();
-        
-        vector<vector<bool>> vis(n+1,vector<bool>(m+1,0));
-        int res = 0;
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        int n=grid.size(),m=grid[0].size();
+        queue<pair<int,pair<int,int>>> q;
+        if(grid[0][0]==1 || grid[n-1][m-1]==1){return -1;}
+        vector<vector<int>> dist(n,vector<int>(m,1e9));
+        q.push({1,{0,0}});
+        dist[0][0]=1;
+        vector<pair<int,int>> dir={{0,1},{0,-1},{-1,0},{1,0},{1,1},{1,-1},{-1,1},{-1,-1}};
 
-        queue<pair<int,int>> q;
-        
-        if(g[0][0] == 0)
-        {
-            q.push({0,0});
-            vis[0][0]=1;
-            
-        }
-        
-        while(!q.empty())
-        {
-            int sz = q.size();
-            res++;
-            
-            for(int ii=0;ii<sz;ii++)
-            {
-                auto p = q.front();
-                int i = p.first;
-                int j = p.second;
-                q.pop();
-                
-                if(i==n-1 && j==m-1)
-                    return res;
+        while(!q.empty()){
+            int dis=q.front().first;
+            auto [i,j]=q.front().second;
+            q.pop();
 
- 
-                if(check(i+1,j,n,m,g,vis))
-                {
-                    q.push({i+1,j});
-                    vis[i+1][j]=1;
+            for(auto it:dir){
+                int x=i+it.first,y=j+it.second;
+                if(x>=0 && x<n && y>=0 && y<m && grid[x][y]==0){
+                    if(dis+1 < dist[x][y]){
+                        dist[x][y]=dis+1;
+                        q.push({dist[x][y],{x,y}});
+                    }
                 }
-                if(check(i-1,j,n,m,g,vis))
-                {
-                    q.push({i-1,j});
-                    vis[i-1][j]=1;
-                }
-                
-                if(check(i,j+1,n,m,g,vis))
-                {
-                    q.push({i,j+1});
-                    vis[i][j+1]=1;
-                }
-                if(check(i,j-1,n,m,g,vis))
-                {
-                    q.push({i,j-1});
-                    vis[i][j-1]=1;
-                }
-                
-                
-                
-                if(check(i+1,j+1,n,m,g,vis))
-                {
-                    q.push({i+1,j+1});
-                    vis[i+1][j+1]=1;
-                }
-                if(check(i+1,j-1,n,m,g,vis))
-                {
-                    q.push({i+1,j-1});
-                    vis[i+1][j-1]=1;
-                }
-                if(check(i-1,j+1,n,m,g,vis))
-                {
-                    q.push({i-1,j+1});
-                    vis[i-1][j+1]=1;
-                }
-                if(check(i-1,j-1,n,m,g,vis))
-                {
-                    q.push({i-1,j-1});
-                    vis[i-1][j-1]=1;
-                }
-
-                
             }
         }
+
+        if(dist[n-1][m-1]!=1e9){
+            return dist[n-1][m-1];
+        }
         return -1;
-        
     }
 };
