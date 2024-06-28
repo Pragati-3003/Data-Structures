2a77a3d4e148c0d2ae3a8d1d9e5d00b841b1937c
Pragati Jain <132778037+Pragati-3003@users.noreply.github.com>
2024-06-28 19:44:38 +0530

    Time: 9 ms (12.35%), Space: 16.2 MB (14.04%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<string>ans;
+    void solve(int open ,int close , string op){
+            if(open==0&&close ==0){
+                ans.push_back(op);
+                  return ;
+            }
+              
+             if(open!=0){
+                 string op1=op;
+                 op1.push_back('(');
+                 solve(open-1,close,op1);
+             }
+             if(close>open){
+                   string op2=op;
+                 op2.push_back(')');
+                 solve(open,close-1,op2);
+             }
+        return;
+    }
+
+    vector<string> generateParenthesis(int n) {
+        string op = "";
+        int open =n;
+        int close = n;
+        solve(open,close,op);
+        return ans;
+    }
+};
