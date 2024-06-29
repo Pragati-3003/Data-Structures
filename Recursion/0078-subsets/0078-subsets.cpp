f71f0bf66d940203d8b66166963dd6ac2587a51a
pragatijain <jpragati3003@gmail.com>
2024-06-29 19:59:51 +0530

    29/06/24_recursion

--- Recursion/0078-subsets/0078-subsets.cpp
+++ Recursion/0078-subsets/0078-subsets.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> res;
-        vector<int> subset;
-
-        createSubset(nums, 0, res, subset);
-        return res;        
-    }
-
-    void createSubset(vector<int>& nums, int index, vector<vector<int>>& res, vector<int>& subset) {
-        if (index == nums.size()) {
-            res.push_back(subset);
+        vector<vector<int>>  ans;
+    void solve(vector<int> nums,vector<int>op){
+        if(nums.size()<=0)
+        {
+            ans.push_back(op);
             return;
         }
-
-        subset.push_back(nums[index]);
-        createSubset(nums, index + 1, res, subset);
-
-        subset.pop_back();
-        createSubset(nums, index + 1, res, subset);
-    }    
+        
+        //not include
+        vector<int> op1= op;
+        //include
+        vector<int>op2=op;
+        op2.push_back(nums[0]);
+        nums.erase(nums.begin()+0);
+        solve(nums,op1);
+        solve(nums,op2);
+        return;
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+       vector<int>op;
+        solve(nums,op);
+        return ans;
+    }
 };
